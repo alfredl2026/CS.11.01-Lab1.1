8db7b878e0db18a0657dd5190ab8cde5d6b232c3
Alfred Lam <pakto1230@gmail.com>
2023-08-31 14:18:52 +0800

    Code added.

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int intAge = ageScanner.nextInt();
+        System.out.println(intAge + "-year olds should date somebody who is at least " + (7 + (intAge / 2)) + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner ageScanner2 = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int intAge2 = ageScanner2.nextInt();
+        System.out.println(intAge2 + "-year olds should read at least " + (100 - intAge2) + " pages before giving up on a book.");
     }
 }
